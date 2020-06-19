8b67c7d26b1a296286d92bed637e7db42ff81c6f
Matheus Vicente <mathewvicente@gmail.com>
2020-06-19 17:56:53 -0300

    Start to create contato section form

--- portifolio/src/components/Contato.js
+++ portifolio/src/components/Contato.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Form, FormGroup, Label, Input, Button, Row, Col } from "reactstrap";
 
 const Contato = () => {
   return <div>
-    <h1 id="contato">Contato</h1>
+    <h1 id="Contato">Contato</h1>
     <Form>
       <FormGroup>
-        <Label for="exampleEmail">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        <Row>
+          <Col md="6">
+            <Label for="exampleEmail">Email</Label>
+            <Input type="email" name="email" id="email" placeholder="E.x.: email@email.com" />
+          </Col>
+          <Col md="6">
+            <Label>Nome:</Label>
+            <Input name="nome" id="nome"/>
+          </Col>
+          <Col md="12">
+            <Label for="exampleText">Mensagem: </Label>
+            <Input type="textarea" name="text" id="mensagem" />
+          </Col>
+          <Col md="6">
+            <Button color="success">Enviar</Button>
+          </Col>
+        </Row>
       </FormGroup>
-      <Button>Submit</Button>
     </Form>
   </div>
 }
