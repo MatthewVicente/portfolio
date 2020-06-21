e95c852a08e28ebd4ed11583da54f31962fc0910
Matheus Vicente <mathewvicente@gmail.com>
2020-06-21 13:24:57 -0300

    Add my description

--- portifolio/src/components/QuemSou.js
+++ portifolio/src/components/QuemSou.js
@@ -7,6 +7,10 @@ const Container = styled.div`
 	flex-direction: column;
 	align-items: center;
 
+	span {
+		color: #ed145b;
+	}
+
 	.foto img {
 		margin: 50px 0;
 		border: 5px solid #ed145b;
@@ -31,10 +35,15 @@ const QuemSou = () => {
 			<img alt="" src={process.env.PUBLIC_URL+"/img/me.jpg"}/>
 		</div>
 		<div className="descricao">
-			Meu nome é Matheus Vicente, sou <span>Game Developer</span>, me graduei em 
-			Tecnologia em Jogos digitias pelo <span>Centro Universitário SENAC Santo Amaro</span>
-			no primeiro semestre de 2020. Durante a faculdade desenvolvi alguns projetos e evolui nas minhas
-			habilidades como programador e como Game Dev.
+			<p>Meu nome é Matheus Vicente, sou <span>Game Developer</span>, me graduei em 
+			Tecnologia em Jogos digitias pelo <span>Centro Universitário SENAC Santo Amaro</span> no 
+			primeiro semestre de 2020. Durante a faculdade desenvolvi alguns projetos sem o uso de engines, outros utilizando da <span>Unity</span> e
+			evolui nas minhas habilidades como programador e como Game Dev.</p> 
+			
+			<p>Em paralelo com a faculdade eu trabalhei como <span>Web Developer</span>,
+			de Janeiro de 2018 até Março de 2020. Aprendi e trabalhei com diversas tecnologias de desenvolvimento Web durante esse período
+			como <span>HTML/CSS</span>, <span>JavaScript</span>, <span>JQuery</span>, <span>ReactJS</span> e <span>Laravel Framework com PHP</span>.
+			Trabalhei desenvolvendo novos sites, fazendo correções de bugs nos sistemas dos clientes da empresa e desenvolvendo novas funcionalidades.</p>
 		</div>
 	</Container>
 }
