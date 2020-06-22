8aaf5dc90bbf16eac174a54b8427ca854137b29d
Matheus Vicente <mathewvicente@gmail.com>
2020-06-22 15:37:53 -0300

    Add EmailJS to project and send email from form and add .env to gitignore

--- portifolio/src/components/Contato.js
+++ portifolio/src/components/Contato.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, FormGroup, Label, Input, Button, Row, Col } from "reactstrap";
 import styled from "styled-components";
+import emailjs from "emailjs-com";
 
 const ContatoContainer = styled.div`
   #contato {
@@ -25,28 +26,58 @@ const ContatoContainer = styled.div`
 `;
 
 const Contato = () => {
+
+  const [contato, setContato] = useState({
+    nome: '',
+    email: '',
+    telefone: '',
+    message: ''
+  });
+
+  const service_id = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+  const  template_id = process.env.REACT_APP_EMAIJS_TEMPLATE_ID;
+  const user_id = process.env.REACT_APP_EMAILJS_USER_ID;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(service_id, template_id, e.target, user_id)
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
+  let handleChange = (e) => {
+    let nome = e.target.name;
+    let value = e.target.value;
+    contato[nome] = value;
+    setContato(contato);
+  }
+
   return <ContatoContainer>
     <h1 id="contato">Contato</h1>
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
         <Row>
           <Col md="6" xs="12">
             <Col md="12">
               <Label for="email">Email:</Label>
-              <Input type="email" name="email" id="email" placeholder="E.x.: email@email.com" />
+              <Input onChange={handleChange} type="email" name="email" id="email" placeholder="E.x.: email@email.com" />
             </Col>
             <Col md="12">
               <Label for="nome">Nome:</Label>
-              <Input name="nome" id="nome"/>
+              <Input onChange={handleChange} name="nome" id="nome" />
             </Col>
             <Col md="12">
               <Label>Telefone:</Label>
-              <Input name="telefone"/>
+              <Input onChange={handleChange} name="telefone" />
             </Col>
           </Col>
           <Col md="6" xs="12">
             <Label for="mensagem">Mensagem: </Label>
-            <Input type="textarea" name="text" id="mensagem" />
+            <Input onChange={handleChange} type="textarea" name="message" id="mensagem" />
           </Col>
           <Col md="12" className="submit-button">
             <Button color="success">Enviar</Button>
