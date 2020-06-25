7b04ba27a13045fd2fcad4747a89baef56151043
Matheus Vicente <mathewvicente@gmail.com>
2020-06-25 10:53:48 -0300

    Updates

---
+++ precache-manifest.a2749a47e52e5c77997b29fc1edaab42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759e8d739ab7b610817167a410b1cd2c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "476f89cd0df7fca55d64",
+    "url": "/portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9f13b8b5a68e5064b763",
+    "url": "/portfolio/static/css/main.ba235319.chunk.css"
+  },
+  {
+    "revision": "476f89cd0df7fca55d64",
+    "url": "/portfolio/static/js/2.052e07fb.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/portfolio/static/js/2.052e07fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f13b8b5a68e5064b763",
+    "url": "/portfolio/static/js/main.3410a0cd.chunk.js"
+  },
+  {
+    "revision": "fb81100604e786b60a00",
+    "url": "/portfolio/static/js/runtime-main.c525fe00.js"
+  }
+]);
