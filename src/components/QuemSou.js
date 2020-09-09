58071720197a9998067af58151fd9aba2a50b057
MatthewVicente <41347019+MatthewVicente@users.noreply.github.com>
2020-09-09 18:22:29 -0300

    Fix typos

--- src/components/QuemSou.js
+++ src/components/QuemSou.js
@@ -36,14 +36,14 @@ const QuemSou = () => {
 		</div>
 		<div className="descricao">
 			<p>Meu nome é Matheus Vicente, sou <span>Game Developer</span>, me graduei em 
-			Tecnologia em Jogos digitias pelo <span>Centro Universitário SENAC Santo Amaro</span> no 
+			Tecnologia em Jogos digitias pelo <span>Centro Universitário SENAC - Santo Amaro</span> no 
 			primeiro semestre de 2020. Durante a faculdade desenvolvi alguns projetos sem o uso de engines, outros utilizando da <span>Unity</span> e
 			evolui nas minhas habilidades como programador e como Game Dev.</p> 
 
-			<p>Trablahei com freelaces utilizando da <span>Unreal Engine</span> desenvolvendo mecânicas utilizando das <span>BluePrints</span>.</p>
+			<p>Trabalhei como freelancer utilizando da <span>Unreal Engine</span>, desenvolvendo mecânicas utilizando das <span>BluePrints</span>.</p>
 			
 			<p>Em paralelo com a faculdade eu trabalhei como <span>Web Developer</span>,
-			de Janeiro de 2018 até Maio de 2020. Aprendi e trabalhei com diversas tecnologias de desenvolvimento Web durante esse período
+			de Janeiro de 2018 até Maio de 2020. Obtive experiência com diversas tecnologias de desenvolvimento Web durante esse período
 			como <span>HTML/CSS</span>, <span>JavaScript</span>, <span>JQuery</span>, <span>ReactJS</span> e <span>Laravel Framework com PHP</span>.
 			Trabalhei desenvolvendo novos sites, fazendo correções de bugs nos sistemas dos clientes da empresa e desenvolvendo novas funcionalidades.</p>
 		</div>
